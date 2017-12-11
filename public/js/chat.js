83b0e4487c81e04d098f5a16e68cff75f42b5a48
khlokesh-184 <khlokesh184@gmail.com>
2017-12-11 18:09:31 +0530

    Join page added

--- public/js/chat.js
+++ public/js/chat.js
@@ -6,6 +6,20 @@ var socket = io();
 			
 		});
 
+		function scrollToBottom(){
+			var messages= jQuery('#messages');
+			var newMessage = messages.children('li:last-child');
+
+			var clientHeight = messages.prop('clientHeight');
+			var scrollTop = messages.prop('scrollTop');
+			var scrollHeight = messages.prop('scrollHeight');
+			var newMessageHeight = newMessage.innerHeight();
+			var lastMessageHeight = newMessage.prev().innerHeight();
+			if(clientHeight+scrollTop+newMessageHeight+lastMessageHeight>=scrollHeight){
+				messages.scrollTop(scrollHeight);
+			}
+		}
+
 		socket.on('disconnect', function() {
 			console.log('disconnected from server');
 		});
@@ -20,7 +34,7 @@ var socket = io();
 			});
 
 			jQuery('#messages').append(html);
-
+			scrollToBottom();
 			// console.log('New Message', message);
 			// var li = jQuery('<li></li>');
 			// li.text(`${message.from} ${formattedTime}: ${message.text}`);
@@ -38,6 +52,7 @@ var socket = io();
 			});
 
 			jQuery('#messages').append(html); 
+			scrollToBottom();
 			// var li = jQuery('<li></li>');
 			// var a = jQuery('<a target="_blank">My current location</a>');
 			
