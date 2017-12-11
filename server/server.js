ac5695ae7091719521c5beb529ea39646fb411b6
khlokesh-184 <khlokesh184@gmail.com>
2017-12-11 13:08:45 +0530

    added CSS

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ io.on('connection', (socket) =>{
 		// socket.emit, emits message to singleuser but
 		// io.emit, emits message to all connected users
 		io.emit('newMessage', generateMessage(message.from, message.text));
-		callback('This is from the server');
+		callback();
 		// socket.broadcast.emit('newMessage', {
 		// 	from: message.from,
 		// 	text: message.text,
